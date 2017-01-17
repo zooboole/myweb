fa6741f6b189c3303eb8e0673138ab14be53dcea
andy brookes <andybrookestar@gmail.com>
2017-01-17 20:39:43 +0000

    using smtp config from config.ini

--- api/controllers/Message.php
+++ api/controllers/Message.php
@@ -1,11 +1,15 @@
 <?php
 class message
      {
-       public $theirName;
-       public $theirEmail;
-       public $theirMessage;
-       public $totalMessage;
-       public $myBoolean;
+       protected $theirName;
+       protected $theirEmail;
+       protected $theirMessage;
+       protected $totalMessage;
+       protected $myBoolean;
+     
+       
+       
+       
        public  function process()
               {
 				  $f3=Base::instance();
@@ -18,10 +22,12 @@ class message
 				$ip = $_SERVER['REMOTE_ADDR'];
 			 	$theIp = strval($ip);
 				$this->totalMessage = "new enquiry  from  ".$this->theirName ." \r\n their ip is    :".$theIp."\r\n their email  ".$this->theirEmail." \r\n their message:  ".$this->theirMessage;
-				 //$smptDetails = $F3->get
-				
-				
-				$smtp = new SMTP ( 'smtp.somehost.com', 587, 'tls/ssl','someemailonhostd@ghanalug.org', 'Somepassword' );
+				$theHost= $f3->get('HOST');
+				$thePort = $f3->get('PORT');
+				$theAuthmethod= $f3->get('authMethod');
+				$emailUserOnDomain=$f3->get('userOnDomain');
+				$emailPass=$f3->get('userPasswordOnDomain');
+				$smtp = new SMTP("$theHost", $thePort,"$theAuthmethod","$emailUserOnDomain","$emailPass");
 				$smtp->set('From', '<andybrookestar@ghanalug.org>');
 				$smtp->set('To', '<andybrookestar@gmail.com>');
 				$smtp->set('Subject', 'Enquiry from ghanalug');  
@@ -40,7 +46,7 @@ class message
 	 
 					  }
     
-    
+                 
     
     
 			}
