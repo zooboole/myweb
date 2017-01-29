8115d1d3b45144bb69e7ee6949571fd3ae58b9c2
andy brookes <andybrookestar@gmail.com>
2017-01-29 20:43:21 +0000

    shifted docs to menu

--- api/controllers/Home.php
+++ api/controllers/Home.php
@@ -9,7 +9,12 @@ class home
 	
 	$f3->set('theTags',"Linux User Group,LUG,Slackware,Distro");
 	        $f3->set('title',  "home page ghanalug.org");	
+				  $code = html_entity_decode("&lsaquo; a href ='/about_us'&rsaquo; click me    &lsaquo;/a  &rsaquo; " );
+				  
+				  $f3->set('randy',$code);
 				  $f3->set('content', "home.htm");			   
+			      
+			  
 			    echo View::instance()->render('page.htm');
 			}
 		
